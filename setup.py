a5d6f4267c675f312dd0e4f21780c653b60ee0cd
Andrew Dunai <a@dun.ai>
2018-05-04 12:27:38 +0300

    Version 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='envaws',
-    version='0.1',
+    version='0.2',
     description='Run application with environment loaded from AWS SSM.',
     author='Andrew Dunai',
     author_email='a@dun.ai',
